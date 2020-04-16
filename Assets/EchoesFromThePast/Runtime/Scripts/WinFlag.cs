e699e9a395ecc7b4eb75c549ddb06c7d0e5c73a2
Record <record.tristan@gmail.com>
2020-04-16 19:48:42 +0200

    Give movement to flag

--- Assets/EchoesFromThePast/Runtime/Scripts/WinFlag.cs
+++ Assets/EchoesFromThePast/Runtime/Scripts/WinFlag.cs
@@ -14,7 +14,7 @@ public class WinFlag : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(0, Mathf.Cos(Time.time*20) / 70, 0);
     }
 
     public void OnTriggerEnter2D(Collider2D collider2D)
