227844f04b27c3a41990d670d90d86a8a086e93b
Record <record.tristan@gmail.com>
2020-04-17 09:57:01 +0200

    Add main title

---
+++ Assets/EchoesFromThePast/Runtime/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    //Play - K-391, AlanWalker
+    public void Play(string nameScene)
+    {
+        SceneManager.LoadScene(nameScene);
+    }
+
+    //Quit - Cashmere Cat, Ariana Grande
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
